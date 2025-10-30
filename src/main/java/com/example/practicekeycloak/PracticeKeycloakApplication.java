9a61ace98c211676921f3cf12d470a9dc484f0d9
hyeon48165 <hyeon48615@gmail.com>
2025-10-31 02:15:30 +0900

    CHORE: 프로젝트 생성

---
+++ src/main/java/com/example/practicekeycloak/PracticeKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicekeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeKeycloakApplication.class, args);
+    }
+
+}
